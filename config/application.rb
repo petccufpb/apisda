a58331484d1dd3368ccc03dd3b57c0846aee0dfc
alvesmarcos <alves.henrique.marcos@gmail.com>
2017-09-16 20:44:36 -0300

    configura CORS

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,12 @@ module Apisda
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :put, :delete, :post, :patch, :options]
+      end
+    end
   end
 end
