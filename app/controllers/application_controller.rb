9d8119913dfdad7e2f55cd4b4b3500f9ee3c8b11
alvesmarcos <alves.henrique.marcos@gmail.com>
2017-09-17 01:06:38 -0300

    adiciona basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Basic::ControllerMethods
+
+  http_basic_authenticate_with name: "sda", password: "senhasda", except: :index
 end
