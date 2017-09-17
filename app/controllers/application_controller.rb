a63cb559a9ca0a1f6ea27de74b9b38d8db999893
alvesmarcos <alves.henrique.marcos@gmail.com>
2017-09-17 12:42:47 -0300

    adiciona env variaveis para senhas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Basic::ControllerMethods
 
-  http_basic_authenticate_with name: "sda", password: "senhasda", except: :index
+  http_basic_authenticate_with name: ENV['AUTH_NAME'], password: ENV['AUTH_PASSWORD'], except: :index
 end
